9d2d58d3e2ef3ea943f3f8a2df07d3fe92dac327
Grisha <grisha2@bk.ru>
2018-07-25 01:42:09 +0300

    reqvest

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Article from './Article'
-import  artikles from '../fixture'
+import ArticleList from './ArticleList'
+import  articles from '../fixture'
 
 function App() {
     return(
         <div>
             <h1>App name</h1>
-            <Article article={artikles[0]}/>
+            <ArticleList articles = {articles}/>
         </div>
     )
 }
